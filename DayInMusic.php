4f20e45b240be47065a3182881b6469f155dd0fb
Carlos Rosão <shemahmforash@gmail.com>
2013-06-12 21:29:41 +0100

    First version of a class that gets ThisDayIn music information.

---
+++ DayInMusic.php
@@ -0,0 +1,49 @@
+<?php
+class DayInMusic {
+    private $_day;
+    private $_month;
+    protected $_source = 'http://www.musicorb.com/day';
+
+    public function __construct( $source = null, $day = null, $month = null ) {
+        $this->_day   = $day ? $day : date("j");
+        $this->_month = $month ? $month : date("F");
+
+        $this->_source = $source ? $source : $this->_source;
+
+        $this->_source = $this->_create_source();
+    }
+
+    protected function _create_source() {
+       return sprintf('%s/%s/%s', $this->_source, $this->_month, $this->_day); 
+    }
+
+    public function getEvents() {
+        $file   = file_get_contents($this->_source);
+        $parser = new HTML_Parser_HTML5( $file );
+
+        return $this->_parse( $parser );
+    }
+
+    protected function _parse( $parser ) {
+        $parser = $parser->root;
+
+        $data = [];
+        foreach($parser('div[itemscope]') as $element) {
+            $year        = $element('span[itemprop=date]', 0);
+            $description = $element('span[itemprop=description]', 0);
+
+            if( !$year && $description ) 
+                continue;
+
+            $year        = $year->getInnerText();
+            $description = $description->getInnerText();
+
+            if( $year && $description ) {
+                $hash = array("year" => $year, "description" => $description );
+                array_push( $data, $hash );
+            }
+        }
+
+        return $data;
+    }
+}
